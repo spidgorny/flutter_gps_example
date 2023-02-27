8377f09267406f1756ad595a1dbf16575ea9d3fa
depidsvy <spidgorny@gmail.com>
2023-02-27 19:56:57 +0100

    - try to fix periodicity

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/ui/with_foreground_task.dart';
 
 import 'service/service_app.dart';
 
 void main() {
   // runApp(const GpsLocationApp());
-  runApp(const ServiceExampleApp());
+  runApp(const WithForegroundTask(child: ServiceExampleApp()));
 }
